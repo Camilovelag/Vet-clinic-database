655ffc1e48644ca39ff8198af5f831bfca05dd3c
Camilovelag <camilo.vela.galindo@gmail.com>
2022-12-08 15:03:04 -0500

    Uncomment queries/schema

--- queries.sql
+++ queries.sql
@@ -1,46 +1,46 @@
--- /*Queries that provide answers to the questions from all projects.*/
--- SELECT * FROM animals WHERE name LIKE '%mon';
--- SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01'AND '2019-12-31';
--- SELECT name FROM animals WHERE neutered = true AND escape_attempts < 3;
--- SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
--- SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
--- SELECT * FROM animals WHERE neutered = true;
--- SELECT * FROM animals WHERE name != 'Gabumon';
--- SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+/*Queries that provide answers to the questions from all projects.*/
+SELECT * FROM animals WHERE name LIKE '%mon';
+SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01'AND '2019-12-31';
+SELECT name FROM animals WHERE neutered = true AND escape_attempts < 3;
+SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+SELECT * FROM animals WHERE neutered = true;
+SELECT * FROM animals WHERE name != 'Gabumon';
+SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
 
--- /* Transactions */
--- BEGIN;
--- UPDATE animals SET species = 'unespecified' WHERE species IS NULL;
--- SELECT * FROM animals;
--- ROLLBACK;
+/* Transactions */
+BEGIN;
+UPDATE animals SET species = 'unespecified' WHERE species IS NULL;
+SELECT * FROM animals;
+ROLLBACK;
 
--- BEGIN;
--- UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
--- UPDATE animals SET species = 'pokemon' WHERE name NOT LIKE '%mon';
--- COMMIT;
--- SELECT * FROM animals;
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE name NOT LIKE '%mon';
+COMMIT;
+SELECT * FROM animals;
 
--- BEGIN;
--- DELETE FROM animals;
--- ROLLBACK;
--- SELECT * FROM animals;
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
 
--- BEGIN;
--- DELETE FROM animals WHERE date_of_birth > '2022-01-01';
--- SAVEPOINT SP1;
--- UPDATE animals SET weight_kg = weight_kg*(-1);
--- ROLLBACK TO SP1;
--- UPDATE animals SET weight_kg = weight_kg*(-1) WHERE weight_kg < 0;
--- COMMIT;
--- SELECT * FROM animals;
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT SP1;
+UPDATE animals SET weight_kg = weight_kg*(-1);
+ROLLBACK TO SP1;
+UPDATE animals SET weight_kg = weight_kg*(-1) WHERE weight_kg < 0;
+COMMIT;
+SELECT * FROM animals;
 
--- /* Answers */
--- SELECT COUNT (*) FROM animals;
--- SELECT COUNT (*) FROM animals WHERE escape_attempts = 0;
--- SELECT AVG (weight_kg) FROM animals;
--- SELECT neutered, MAX (escape_attempts) FROM animals GROUP BY neutered;
--- SELECT species, MAX (weight_kg), MIN (weight_kg) FROM animals GROUP BY species;
--- SELECT species, AVG (escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
+/* Answers */
+SELECT COUNT (*) FROM animals;
+SELECT COUNT (*) FROM animals WHERE escape_attempts = 0;
+SELECT AVG (weight_kg) FROM animals;
+SELECT neutered, MAX (escape_attempts) FROM animals GROUP BY neutered;
+SELECT species, MAX (weight_kg), MIN (weight_kg) FROM animals GROUP BY species;
+SELECT species, AVG (escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
 
 /* Queries related to owners and species */
 SELECT A.name, O.full_name FROM animals A INNER JOIN owners O ON A.owner_id = O.id WHERE O.full_name = 'Melody Pond';
