736f873c37e60ad54389abc445d510782dbaa1bc
Camilovelag <camilo.vela.galindo@gmail.com>
2022-12-08 17:31:36 -0500

    Insert data in vets, specializations, and visits

--- data.sql
+++ data.sql
@@ -34,3 +34,61 @@ UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Jennifer
 UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Bob') WHERE name = 'Devimon' OR name = 'Plantmon';
 UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Melody Pond') WHERE name = 'Charmander' OR name = 'Squirtle' OR name = 'Blossom';
 UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Dean Winchester') WHERE name = 'Angemon' OR name = 'Boarmon';
+
+/* Insert data into vets table */
+INSERT INTO vets (name, age, date_of_graduation) VALUES ('William Tatcher', 45, '2000-04-23');
+INSERT INTO vets (name, age, date_of_graduation) VALUES ('Maisy Smith', 26, '2019-01-17');
+INSERT INTO vets (name, age, date_of_graduation) VALUES ('Stephanie Mendez', 64, '1981-05-04');
+INSERT INTO vets (name, age, date_of_graduation) VALUES ('Jack Harkness', 38, '2008-06-08');
+
+/* Insert data into specializations table */
+INSERT INTO specializations (vet_id, species_id)
+  VALUES ((SELECT id FROM vets WHERE name = 'William Tatcher'), (SELECT id FROM species WHERE name = 'Pokemon'));
+INSERT INTO specializations (vet_id, species_id)
+  VALUES ((SELECT id FROM vets WHERE name = 'Stephanie Mendez'), (SELECT id FROM species WHERE name = 'Digimon'));
+INSERT INTO specializations (vet_id, species_id)
+  VALUES ((SELECT id FROM vets WHERE name = 'Stephanie Mendez'), (SELECT id FROM species WHERE name = 'Pokemon'));
+INSERT INTO specializations (vet_id, species_id)
+  VALUES ((SELECT id FROM vets WHERE name = 'Jack Harkness'), (SELECT id FROM species WHERE name = 'Digimon'));
+
+/* Insert data into visits table */
+INSERT INTO visits (animal_id, vet_id, date_of_visit)
+  VALUES ((SELECT id FROM animals WHERE name = 'Agumon'), (SELECT id FROM vets WHERE name = 'William Tatcher'), '2020-05-24');
+INSERT INTO visits (animal_id, vet_id, date_of_visit)
+  VALUES ((SELECT id FROM animals WHERE name = 'Agumon'), (SELECT id FROM vets WHERE name = 'Stephanie Mendez'), '2020-07-22');
+INSERT INTO visits (animal_id, vet_id, date_of_visit)
+  VALUES ((SELECT id FROM animals WHERE name = 'Gabumon'), (SELECT id FROM vets WHERE name = 'Jack Harkness'), '2021-02-02');
+INSERT INTO visits (animal_id, vet_id, date_of_visit)
+  VALUES ((SELECT id FROM animals WHERE name = 'Pikachu'), (SELECT id FROM vets WHERE name = 'Maisy Smith'), '2020-01-05');
+INSERT INTO visits (animal_id, vet_id, date_of_visit)
+  VALUES ((SELECT id FROM animals WHERE name = 'Pikachu'), (SELECT id FROM vets WHERE name = 'Maisy Smith'), '2020-03-08');
+  INSERT INTO visits (animal_id, vet_id, date_of_visit)
+  VALUES ((SELECT id FROM animals WHERE name = 'Pikachu'), (SELECT id FROM vets WHERE name = 'Maisy Smith'), '2020-05-14');
+INSERT INTO visits (animal_id, vet_id, date_of_visit)
+  VALUES ((SELECT id FROM animals WHERE name = 'Devimon'), (SELECT id FROM vets WHERE name = 'Stephanie Mendez'), '2020-05--04');
+INSERT INTO visits (animal_id, vet_id, date_of_visit)
+  VALUES ((SELECT id FROM animals WHERE name = 'Charmander'), (SELECT id FROM vets WHERE name = 'Jack Harkness'), '2021-02-24');
+INSERT INTO visits (animal_id, vet_id, date_of_visit)
+  VALUES ((SELECT id FROM animals WHERE name = 'Plantmon'), (SELECT id FROM vets WHERE name = 'Maisy Smith'), '2021-03-22');
+INSERT INTO visits (animal_id, vet_id, date_of_visit)
+  VALUES ((SELECT id FROM animals WHERE name = 'Plantmon'), (SELECT id FROM vets WHERE name = 'William Tatcher'), '2020-08-10');
+INSERT INTO visits (animal_id, vet_id, date_of_visit)
+  VALUES ((SELECT id FROM animals WHERE name = 'Plantmon'), (SELECT id FROM vets WHERE name = 'Maisy Smith'), '2021-04-07');
+INSERT INTO visits (animal_id, vet_id, date_of_visit)
+  VALUES ((SELECT id FROM animals WHERE name = 'Squirtle'), (SELECT id FROM vets WHERE name = 'Stephanie Mendez'), '2019-09-29');
+INSERT INTO visits (animal_id, vet_id, date_of_visit)
+  VALUES ((SELECT id FROM animals WHERE name = 'Angemon'), (SELECT id FROM vets WHERE name = 'Jack Harkness'), '2020-10-03');
+INSERT INTO visits (animal_id, vet_id, date_of_visit)
+  VALUES ((SELECT id FROM animals WHERE name = 'Angemon'), (SELECT id FROM vets WHERE name = 'Jack Harkness'), '2020-11-04');
+INSERT INTO visits (animal_id, vet_id, date_of_visit)
+  VALUES ((SELECT id FROM animals WHERE name = 'Boarmon'), (SELECT id FROM vets WHERE name = 'Maisy Smith'), '2019-01-24');
+INSERT INTO visits (animal_id, vet_id, date_of_visit)
+  VALUES ((SELECT id FROM animals WHERE name = 'Boarmon'), (SELECT id FROM vets WHERE name = 'Maisy Smith'), '2019-05-15');
+INSERT INTO visits (animal_id, vet_id, date_of_visit)
+  VALUES ((SELECT id FROM animals WHERE name = 'Boarmon'), (SELECT id FROM vets WHERE name = 'Maisy Smith'), '2020-02-27');
+INSERT INTO visits (animal_id, vet_id, date_of_visit)
+  VALUES ((SELECT id FROM animals WHERE name = 'Boarmon'), (SELECT id FROM vets WHERE name = 'Maisy Smith'), '2020-08-03');
+INSERT INTO visits (animal_id, vet_id, date_of_visit)
+  VALUES ((SELECT id FROM animals WHERE name = 'Blossom'), (SELECT id FROM vets WHERE name = 'Stephanie Mendez'), '2020-05-24');
+INSERT INTO visits (animal_id, vet_id, date_of_visit)
+  VALUES ((SELECT id FROM animals WHERE name = 'Blossom'), (SELECT id FROM vets WHERE name = 'William Tatcher'), '2021-01-11');
--- schema.sql
+++ schema.sql
@@ -1,34 +1,35 @@
--- /* Database schema to keep the structure of entire database. */
+/* Database schema to keep the structure of entire database. */
 
--- CREATE TABLE animals (
---   id INT GENERATED ALWAYS AS IDENTITY,
---   name VARCHAR(100),
---   date_of_birth DATE,
---   escape_attempts INT,
---   neutered BOOLEAN,
---   weight_kg DECIMAL);
+CREATE TABLE animals (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(100),
+  date_of_birth DATE,
+  escape_attempts INT,
+  neutered BOOLEAN,
+  weight_kg DECIMAL);
 
---   ALTER TABLE animals
---   ADD COLUMN species VARCHAR(100);
+  ALTER TABLE animals
+  ADD COLUMN species VARCHAR(100);
 
--- /* Tables for owners and species. */
+/* Tables for owners and species. */
 
--- CREATE TABLE owners (
---   id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
---   full_name VARCHAR(100),
---   age INT);
+CREATE TABLE owners (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  full_name VARCHAR(100),
+  age INT);
 
--- CREATE TABLE species (
---   id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
---   name VARCHAR(100));
+CREATE TABLE species (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name VARCHAR(100));
 
---   ALTER TABLE animals DROP COLUMN species;
---   ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species (id);
---   ALTER TABLE animals ADD FOREIGN KEY (species_id) REFERENCES species (id);
---   ALTER TABLE animals ADD COLUMN owner_id INT REFERENCES owners (id);
---   ALTER TABLE animals ADD FOREIGN KEY (owner_id) REFERENCES owners (id);
+  ALTER TABLE animals DROP COLUMN species;
+  ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species (id);
+  ALTER TABLE animals ADD FOREIGN KEY (species_id) REFERENCES species (id);
+  ALTER TABLE animals ADD COLUMN owner_id INT REFERENCES owners (id);
+  ALTER TABLE animals ADD FOREIGN KEY (owner_id) REFERENCES owners (id);
+  ALTER TABLE animals ADD PRIMARY KEY (id);
 
---   ALTER TABLE animals ADD PRIMARY KEY (id);
+/* Tables for vets, specializations, and visits. */
 
 CREATE TABLE vets (
   id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
