f9b5e57c724615173aa229f70b2676a468f9b813
Camilovelag <camilo.vela.galindo@gmail.com>
2022-12-07 20:15:14 -0500

    Add answers

--- queries.sql
+++ queries.sql
@@ -27,11 +27,20 @@
 -- ROLLBACK;
 -- SELECT * FROM animals;
 
-BEGIN;
-DELETE FROM animals WHERE date_of_birth > '2022-01-01';
-SAVEPOINT SP1;
-UPDATE animals SET weight_kg = weight_kg*(-1);
-ROLLBACK TO SP1;
-UPDATE animals SET weight_kg = weight_kg*(-1) WHERE weight_kg < 0;
-COMMIT;
-SELECT * FROM animals;
+-- BEGIN;
+-- DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+-- SAVEPOINT SP1;
+-- UPDATE animals SET weight_kg = weight_kg*(-1);
+-- ROLLBACK TO SP1;
+-- UPDATE animals SET weight_kg = weight_kg*(-1) WHERE weight_kg < 0;
+-- COMMIT;
+-- SELECT * FROM animals;
+
+/* Answers */
+
+SELECT COUNT (*) FROM animals;
+SELECT COUNT (*) FROM animals WHERE escape_attempts = 0;
+SELECT AVG (weight_kg) FROM animals;
+SELECT neutered, MAX (escape_attempts) FROM animals GROUP BY neutered;
+SELECT species, MAX (weight_kg), MIN (weight_kg) FROM animals GROUP BY species;
+SELECT species, AVG (escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
